100e0f166d921a17075c11d6875f0f10b017d4f0
hijyou <peppehitagi0607@docomo.ne.jp>
2021-11-25 10:32:13 +0900

    最初のpush

---
+++ src/components/FirebaseApp.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>Firestoreを使う</h1>
+    <div>
+      <input v-model="message">
+      <button v-on:click="postMessage">メッセージをFirestoreに保存する</button>
+      <div v-for="(message, index) in messages" v-bind:key="index">
+        {{ message.text }}
+      </div>
+      <button v-on:click="postTest">ハローを追加</button>
+      <button v-on:click="getHello">ハローを検索</button>
+    </div>
+  </div>
+</template>
+>
+<script>
+import firebase from "firebase"
+
+
+
+export default {
+  data() {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    postMessage() {
+      const data = { text: this.message }
+      firebase
+        .firestore()
+        .collection("messages")
+        .add(data)
+        .then(() => {
+          this.messages.push(data)
+        })
+    },
+    postTest() {
+      const data = { text: 'ハロー'}
+      firebase.firestore().collection("test").doc("0").set(data)
+    },
+    getHello() {
+      firebase.firestore()
+      .collection("messages")
+      .where("text" ,"==", "こんにちは、メッセージの本文です。")
+      // .orderBy("text", "desc")
+      .limit(1)
+      .get()
+      .then((snapshot)=> {
+        snapshot.forEach((doc) => {
+          console.log(doc.id, "=>", doc.data())
+        })
+      })
+    }
+  },
+  created() {
+    firebase
+      .firestore()
+      .collection("messages")
+      .get()
+      .then((snapshot) => {
+        for (let i = 0; i < snapshot.docs.length; i++) {
+          this.messages.push(snapshot.docs[i].data())
+        }
+      })
+  },
+}
+</script>
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1xkuUFLKrvgLLACo6zgaXqHpdGrK6O8U",
+  authDomain: "my-project-8ec10.firebaseapp.com",
+  projectId: "my-project-8ec10",
+  storageBucket: "my-project-8ec10.appspot.com",
+  messagingSenderId: "750697718753",
+  appId: "1:750697718753:web:3fb5053b24eb4f08d0e8a0",
+  measurementId: "G-W3M96B3W4J",
+}
+
+firebase.initializeApp(firebaseConfig)
