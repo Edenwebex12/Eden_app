1cb142d21c9a859fbc6ef1f3b8c4286deeff3ec5
hijyou <peppehitagi0607@docomo.ne.jp>
2021-11-25 14:38:45 +0900

    練習

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <div><FirebaseApp /></div>
-    <div><ExampleApp /></div>
   </div>
 </template>
 
--- src/components/FirebaseApp.vue
+++ src/components/FirebaseApp.vue
@@ -2,21 +2,17 @@
   <div>
     <h1>Firestoreを使う</h1>
     <div>
-      <input v-model="message">
+      <input v-model="message" />
       <button v-on:click="postMessage">メッセージをFirestoreに保存する</button>
       <div v-for="(message, index) in messages" v-bind:key="index">
         {{ message.text }}
       </div>
-      <button v-on:click="postTest">ハローを追加</button>
-      <button v-on:click="getHello">ハローを検索</button>
     </div>
   </div>
 </template>
 >
 <script>
 import firebase from "firebase"
-
-
 
 export default {
   data() {
@@ -36,22 +32,23 @@ export default {
         })
     },
     postTest() {
-      const data = { text: 'ハロー'}
+      const data = { text: "ハロー" }
       firebase.firestore().collection("test").doc("0").set(data)
     },
     getHello() {
-      firebase.firestore()
-      .collection("messages")
-      .where("text" ,"==", "こんにちは、メッセージの本文です。")
-      // .orderBy("text", "desc")
-      .limit(1)
-      .get()
-      .then((snapshot)=> {
-        snapshot.forEach((doc) => {
-          console.log(doc.id, "=>", doc.data())
+      firebase
+        .firestore()
+        .collection("messages")
+        .where("text", "==", "こんにちは、メッセージの本文です。")
+        // .orderBy("text", "desc")
+        .limit(1)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            console.log(doc.id, "=>", doc.data())
+          })
         })
-      })
-    }
+    },
   },
   created() {
     firebase
