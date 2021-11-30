6ce461033224724a976862b49c02ccbd54631ce2
chiakisite <chiakik0522@gmail.com>
2021-11-30 22:25:33 +0900

    上の方を実装

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,5 +1,83 @@
 <template>
   <div id="top">
-    <div>日記</div>
+    <div class="title"><h2>日記</h2></div>
+    <div><NavBar /></div>
+    <div class="posts"></div>
+    <div v-for="(message, index) in messages" v-bind:key="index">
+      {{ message.text }}
+    </div>
   </div>
 </template>
+
+<script>
+import NavBar from "@/components/NavBar"
+import firebase from "firebase"
+
+export default {
+  name: "top",
+  components: {
+    NavBar,
+    firebase,
+  },
+  data() {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    postMessage() {
+      const data = { text: this.message }
+      firebase
+        .firestore()
+        .collection("messages")
+        .add(data)
+        .then(() => {
+          this.messages.push(data)
+        })
+    },
+    postTest() {
+      const data = { text: "ハロー" }
+      firebase.firestore().collection("test").doc("0").set(data)
+    },
+    getHello() {
+      firebase
+        .firestore()
+        .collection("messages")
+        .where("text", "==", "こんにちは、メッセージの本文です。")
+        // .orderBy("text", "desc")
+        .limit(1)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            console.log(doc.id, "=>", doc.data())
+          })
+        })
+    },
+  },
+  created() {
+    firebase
+      .firestore()
+      .collection("messages")
+      .get()
+      .then((snapshot) => {
+        for (let i = 0; i < snapshot.docs.length; i++) {
+          this.messages.push(snapshot.docs[i].data())
+        }
+      })
+  },
+}
+</script>
+
+<style>
+#top {
+  display: flex;
+  flex-direction: column;
+  align-items: space-between;
+}
+
+.title {
+  display: flex;
+  justify-content: flex-start;
+  padding-left: 10%;
+}
+</style>
